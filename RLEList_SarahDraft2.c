d2d6965b4c8ca6a229a45c3ff79f7e923fb9beb7
s-griffith <cmcgriffith@gmail.com>
2022-04-25 19:54:58 +0300

    Overall Editing - especially get and remove functions. Helper functions for string function.

--- RLEList_SarahDraft2.c
+++ RLEList_SarahDraft2.c
@@ -1,8 +1,15 @@
 #include "RLEList.h"
 #include <stdlib.h>
-#define NUM_OF_CATEGORIES 3
+#include <stdio.h>
+#define NUM_OF_CATEGORIES 2
+#define CONVERT_TO_ASCII 48
+#define BASE_TEN 10
+
 
 RLEList findIndex (RLEList list, int index);
+int translateToString (int appears, int index, char* string);
+int numOfNodes (RLEList list);
+int numOfDigits (RLEList list);
 
 struct RLEList_t{
     char symbol;
@@ -11,7 +18,7 @@ struct RLEList_t{
 };
 
 RLEList RLEListCreate() {
-    RLEList node = malloc(sizeof(*node));
+    RLEList node = (RLEList)malloc(sizeof(*node));
     if (node == NULL) {
         return NULL;
     }
@@ -30,7 +37,7 @@ void RLEListDestroy (RLEList list) {
 }
 
 RLEListResult RLEListAppend (RLEList list, char value) {
-    if (list == NULL)  { //how do you check that value is not null? if you do value == NULL, it gives an error 
+    if (list == NULL || value == NULL)  { //try inputting 0 as a scanf
         return RLE_LIST_NULL_ARGUMENT;
     }
     if (list->appears == 0) {
@@ -62,84 +69,148 @@ int RLEListSize (RLEList list) {
     int counter = 0;
     while (list != NULL) {
         counter += list->appears;
-        list = list ->next;
+        list = list->next;
     }
     return counter;
 }
 
+//What happens where there's only one singular node? What if you want to destroy the first in a chain? What if you want to delete one of many inside a single node that is also the first/only?
 RLEListResult RLEListRemove (RLEList list, int index) {
-    //Does the index count start at 1 or 0? From Piazza - search "remove"
     if (list == NULL) {
         return RLE_LIST_NULL_ARGUMENT;
     }
     if (index <= 0) {
         return RLE_LIST_INDEX_OUT_OF_BOUNDS;
     }
-    list = findIndex(list, index);
-    if (list == NULL) {
+    RLEList previous = findIndex(list, index);
+    if (previous == NULL) {
+        previous = list;
+ //       if (previous->next == NULL) {
+ //           RLEListDestroy(list);
+     //   }
+    }
+    RLEList delete = previous->next;
+    if (delete == NULL) {
         return RLE_LIST_INDEX_OUT_OF_BOUNDS;
     }
-    if (list->appears == 1) {
-        RLEList temp =list->next;
-        list->next=temp->next;
-        temp->next = NULL;
-        RLEListDestroy(temp);
+    if (delete->appears == 1) {
+        previous->next=delete->next;
+        delete->next = NULL;
+        RLEListDestroy(delete);
     }
     else {
         list->appears--;
     }
     return RLE_LIST_SUCCESS;
+    //if want to remove the last node? if there is only one node?
 }
 
-char RLEListGet (RLEList list, int index, RLEListResult *result) { //if what is null the result will not be saved??
-    if (list == NULL) {
+char RLEListGet (RLEList list, int index, RLEListResult *result) {
+    if (list == NULL && result != NULL) {
         *result = RLE_LIST_NULL_ARGUMENT;
     }
-    else if (index <= 0) {
+    else if (index <= 0 && result != NULL) {
         *result = RLE_LIST_INDEX_OUT_OF_BOUNDS;
     }
-    else {
-        list = findIndex(list, index);
-        if (list == NULL) {
+    else if (list != NULL && index > 0) {
+        RLEList current = findIndex(list, index);
+        if (current == NULL) {
+            current == list;
+        }
+        else {
+            current = current->next;
+        }
+        if (current == NULL && result != NULL) {
             *result = RLE_LIST_INDEX_OUT_OF_BOUNDS;
         }
-        *result = RLE_LIST_SUCCESS;
-        return list->symbol;
+        else {
+            if (result != NULL) {
+                *result = RLE_LIST_SUCCESS;
+            }
+            return current->symbol;
+        } 
     }
     return 0;
 }
 
-char* RLEListExportToString (RLEList list, RLEListResult* result) { //if what is null, the result won't be saved?
+char* RLEListExportToString (RLEList list, RLEListResult* result) {
     if (list == NULL) {
-        *result = RLE_LIST_NULL_ARGUMENT;
+        if (result != NULL) {
+            *result = RLE_LIST_NULL_ARGUMENT;
+        }
         return NULL;
     }
-    int size = NUM_OF_CATEGORIES*RLEListSize(list)+1;
-    char *string = malloc(sizeof(char)*size); //free! - by user
+    int size = numOfDigits(list) + NUM_OF_CATEGORIES*numOfNodes(list) + 1;
+    char *string = (char*)malloc(sizeof(char)*size); //free! - by user
     if (string == NULL) {
-        *result = RLE_LIST_OUT_OF_MEMORY;
+        if (result != NULL) {
+            *result = RLE_LIST_OUT_OF_MEMORY;
+        }
         return NULL;
     }
     int index = 0;
     while (list != NULL) {
-        string[index] = list->symbol;
-        string[++index] = list->appears;
-        string[++index] = '\n';
+        string[index++] = list->symbol;
+        index = translateToString(list->appears, index, string);
+        string[index] = '\n';
         index++;
+        list = list->next;
     }
     string[index] = '\0';
-    *result = RLE_LIST_SUCCESS;
+    if (result != NULL) {
+        *result = RLE_LIST_SUCCESS;
+    }
     return string;
  }
 
- RLEList findIndex (RLEList list, int index) {
+RLEList findIndex (RLEList list, int index) {
+    RLEList previous = NULL;
     while (list != NULL) {
         index -= list->appears;
         if(index <= 0) {
             break;
         }
+        previous = list;
         list = list->next;
     }
-    return list;
+    return previous;
  }
 
+int numOfDigits (RLEList list) {
+    int appearsTemp = 0, counter = 0;
+    RLEList current = list;
+    while (current) {
+        appearsTemp = current->appears;
+        while (appearsTemp > 0) {
+            appearsTemp /= BASE_TEN;
+            counter++;
+        }
+        current = current->next;
+    }
+    return counter;
+}
+
+int translateToString (int appears, int index, char* string) {
+    int first = index;
+    char temp;
+    while (appears > 0) {
+        string[index] = (appears % BASE_TEN) + CONVERT_TO_ASCII;
+        appears /= BASE_TEN;
+        index++;
+    }
+    for (int i = 0; i < (index-first)/2; i++) {
+        temp = string[first+i];
+        string[first] == string[index-i-1];
+        string[index-i-1]  = temp;
+    }
+    return index; //the next open index in the string
+}
+
+int numOfNodes (RLEList list) {
+    int counter = 0;
+    while (list != NULL) {
+        counter++;
+        list = list->next;
+    }
+    return counter;
+}
